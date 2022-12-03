56602b3ee664a26bb6860a00d7449cedbc14f072
Luciano Medernach <luciano.medernach@gmail.com>
2022-12-02 21:15:15 -0300

    Angular form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { AboutMeComponent } from './about-me/about-me.component';
@@ -10,10 +11,21 @@ import { EducationComponent } from './education/education.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ProyectsComponent } from './proyects/proyects.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, AboutMeComponent, ExperienceComponent, EducationComponent, SkillsComponent, ProyectsComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    AboutMeComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProyectsComponent,
+    LoginComponent,
+    HomeComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  warning = '';
 
+  onSubmit() {
+    console.warn(this.loginForm.value);
+  }
+
+  loginForm = new FormGroup({
+    user: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(16),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(64),
+    ]),
+  });
+
+  get user() {
+    return this.loginForm.get('user');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
 }
